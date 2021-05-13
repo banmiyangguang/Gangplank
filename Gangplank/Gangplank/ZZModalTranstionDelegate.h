e7ff0fcc964d7d7d8c0d5c0631b11a361a8f414a
张志豪 <zhangzhihao.lucas@bytedance.com>
2021-05-13 18:09:29 +0800

    test commit

---
+++ Gangplank/Gangplank/ZZModalTranstionDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ZZModalTranstionDelegate.h
+//  Gangplank
+//
+//  Created by Zhihao Zhang on 2021/5/12.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZZModalTranstionDelegate : NSObject <UIViewControllerTransitioningDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
